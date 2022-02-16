ad72701a5303627d29769662316c7fefb6ce8e1e
pranu06 <99705961+pranu06@users.noreply.github.com>
2022-02-15 20:52:29 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,19 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <title>Student Details</title>
   </head>
-  <body style="background-color: #f1f1f1;">
-      <div class="text-center p-5">
-        <h5><i>"Simple Static Website Assignment Spring 2022 using Cloud Managed Web Service"</i></h5>
+  <body>
+      <div>
+        <h3>"Simple Static Website Assignment Spring 2022 using Cloud Managed Web Service"</h5>
         <h5>Name: Madhulika Musku</h5>
         <h5>Student ID: 16320561</h5>
         <h5>Cloud Provider: Heroku</h5>
 
       </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+ 
+ 
   </body>
 </html>
